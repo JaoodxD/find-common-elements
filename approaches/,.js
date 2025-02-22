575ae88ac1d81d497bbf7544e61301a3f38c26c7
JaoodxD <jaood96@gmail.com>
2025-02-22 22:55:52 +0200

    Revert initial in-place sorting

--- approaches/,.js
+++ approaches/,.js
@@ -1,8 +1,7 @@
 export default function panComa (a, v) {
   var sortfn=(l,r)=>l<r?-1:1;
-  a = a.toSorted(sortfn);
-  v = v.toSorted(sortfn);
-  // console.log(a,v);
+  a.sort(sortfn);
+  v.sort(sortfn);
   var i=0,j=0,r=[a[0]-1];
   while (i<a.length&&j<v.length){
      a[i]==v[j]?(
